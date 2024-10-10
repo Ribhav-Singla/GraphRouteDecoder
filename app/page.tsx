021531be0c5162c9f3ffd4ac48e067c93eff9dcf
ribhavsingla <ribhavsingla2166@gmail.com>
2024-10-10 18:52:33 +0530

    removed fontawesome script

--- app/page.tsx
+++ app/page.tsx
@@ -20,16 +20,6 @@ export default function Home() {
   const path = useRecoilValue(pathState);
 
   useEffect(() => {
-    const loadFontAwesome = () => {
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href =
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
-      document.head.appendChild(link);
-    };
-
-    loadFontAwesome(); // Load FontAwesome
-
     const script = document.createElement("script");
     script.src =
       "https://unpkg.com/vis-network/standalone/umd/vis-network.min.js";
